2c11b2880d5864fdcb32b9f1ee6065fcfe2777c8
Kyungjae Lee <kjxlee@gmail.com>
2023-07-19 20:08:43 +0100

    Update 'workspace/host/host.py' - Insert code to resolve 'mass-erase' error

--- workspace/host/host.py
+++ workspace/host/host.py
@@ -410,6 +410,8 @@ def decode_menu_command_code(command):
         sector_num = int(sector_num, 16)
         if(sector_num != 0xff):
             nsec=int(input("\n   Enter number of sectors to erase(max 8) here :"))
+        else:
+            nsec=0  # Added by Kyungjae Lee to resolve an issue when sector_num=0xff
         
         data_buf[2]= sector_num 
         data_buf[3]= nsec 
